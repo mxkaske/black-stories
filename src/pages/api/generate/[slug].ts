749c3d4b11f7cf59be42848033bc4a6fbffa1dc8
Maximilian Kaske <maximilian@kaske.org>
2023-02-18 21:01:56 +0100

    refactor: remove openai sdk and replace with edge function with set-cookie

--- src/pages/api/generate/[slug].ts
+++ src/pages/api/generate/[slug].ts
@@ -1,59 +1,101 @@
-import { createCompletion } from "@/lib/openai";
-import { NextApiRequest, NextApiResponse } from "next";
 import { redis } from "@/lib/upstash";
 import { generatePromptBySlug, promptKeyBySlug } from "@/lib/prompt";
-// import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
 
 // REMINDER: timeout for serverless functions on vercel hobby plan is 10 seconds
 // REMINDER: timeout for edge functions on vercel hobby plan is 30 seconds
-// DISCUSS: whether to move function to the edge or not! BUT: axios error on the edge if using openai node sdk
-// the solution would be to make an HTTP API request instead
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("Missing Environment Variable OPENAI_API_KEY");
+}
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+const handler = async (req: NextRequest) => {
   try {
-    const { slug } = req.query as { slug: string };
-    // TODO: SWITCH WITH THE NEW app/route.ts DEFINITION
-    // const cookieStore = cookies();
-    // const token = cookieStore.get("token");
-    // const uuid = token?.value || crypto.randomUUID();
+    // REMINDER: if cookie does not exist, create a random uuid
+    const token = req.cookies.get("token")?.value || crypto.randomUUID();
+    const { searchParams } = new URL(req.url);
+    const slug = searchParams.get("slug");
+    if (!slug) {
+      return new Response("Bad Request", { status: 400 });
+    }
+    // TODO: add token
     const key = promptKeyBySlug(slug);
+
     switch (req.method) {
       case "POST": {
-        const { question } = req.body as {
-          question?: string;
-        };
-
+        const { question } = (await req.json()) as { question?: string };
         if (!question) {
-          return res.status(400).end("Bad Request");
+          return new Response("Bad Request", { status: 400 });
         }
 
         const prompt = generatePromptBySlug(slug, question);
-        const response = await createCompletion(prompt);
+
+        const payload = {
+          model: "text-davinci-003",
+          prompt,
+          temperature: 0.7,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+          max_tokens: 2048,
+          n: 1,
+        };
+
+        const response = await fetch("https://api.openai.com/v1/completions", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+          },
+          method: "POST",
+          body: JSON.stringify(payload),
+        });
+
+        if (!response.body) {
+          return new Response("Bad Request", { status: 500 });
+        }
+
+        const json = await response.json();
         // `data` = "answer,significance"
-        const data = response.data.choices[0].text;
+        const data = json.choices[0].text;
+
         const [answer, significance] = data?.split(",") || ["N/A", 0];
-        console.log({ data, answer, significance });
 
         // append to redis chat history
         await redis.zadd(key, {
           score: Date.now(),
           member: { question, answer, significance },
         });
+
         // expire key after 10 hours
         await redis.expire(key, 36000); // 60s * 60min * 10h
 
-        return res.json({ data });
+        return new Response(
+          JSON.stringify({ question, answer, significance }),
+          {
+            headers: {
+              "Content-Type": "application/json; charset=utf-8",
+              "Set-Cookie": `token=${token}; Max-Age=${36000}; Path=${"/"}`,
+            },
+          }
+        );
       }
       case "DELETE": {
         await redis.del(key);
-        return res.status(202).end();
+        return new Response("Deleted", { status: 202 });
       }
       default: {
-        return res.status(405).end("Method Not Allowed");
+        return new Response("Method Not Allowed", { status: 405 });
       }
     }
   } catch (error) {
-    return res.status(500).json({ error });
+    return new Response(JSON.stringify({ error }), {
+      status: 500,
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+    });
   }
 };
 
